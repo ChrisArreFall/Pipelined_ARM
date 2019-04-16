723786ed699a894bb28bbed14bc50b9cf7116271
Christopher Arredondo <chrisarrefall@gmail.com>
2019-04-16 04:18:04 -0600

    Hazard Unit skeleton

---
+++ Hazard Unit/hazardUnit.sv
@@ -0,0 +1,5 @@
+module hazardUnit(input  logic [4:0]  match,
+					  input  logic	RegWriteM, RegWriteW, MemtoRegE, 
+					  output logic [1:0]  forwardAE, forwardBE,
+					  output logic stallD, stallF, flushD, flushE);					 
+endmodule 
